5e6fabf18583ae5cfad216fd5a49e51d19abc1ff
addinkevin@gmail.com <addinkevin@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-05-25 01:52:05 +0000

    Creacion del proyecto

---
+++ trunk/BatallaNavalgo/BatallaNavalgo/Saludador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BatallaNavalgo
+{
+    class Saludador
+    {
+        public static string Saludar()
+        {
+            return "Hola mundo!!!";
+        }
+
+        public static void Main ( string[] args)
+        {
+            Saludador.Saludar();
+            
+        }
+    }
+}
