8d8d3db7620aa7cc5eff1543f7cde159a10467fd
MPrystupiuk@gmail.com <MPrystupiuk@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-06-07 02:41:17 +0000

    Pase un poco de lo que estaba en los diagramas UML.

---
+++ BatallaNavalgo/Armamento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Batalla_Navalgo
+{
+    abstract class Armamento
+    {
+        protected Posicion posicion;
+        protected int costo;
+        protected Boolean explotado;       
+
+        public void Atacar(Atacable i)
+        {
+            
+        }
+        //-----------------------------------------------------------
+
+        public void Actualizar()
+        {
+
+        }
+        //-----------------------------------------------------------
+
+        public Boolean EstaExplotado()
+        {
+            return this.explotado;
+        }
+        //-----------------------------------------------------------
+
+    }
+}
