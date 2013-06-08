32b564ef4b72d4e595c8a207e221a5cdf30aa917
fernandezsantid@gmail.com <fernandezsantid@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-06-08 19:57:07 +0000

    Update de la clase Tablero y agregue TableroTest

---
+++ trunk/BatallaNavalgo/BatallaNavalgoTests/TableroTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BatallaNavalgo;
+using NUnit.Framework;
+
+namespace BatallaNavalgoTests
+{
+    [TestFixture]
+    class TableroTest
+    {
+        [Test]
+        public void testDeberiaEstarVacioEnElMomentoDeLaCreacion()
+        {
+            Tablero tablero = new Tablero();
+
+            Assert.True(tablero.EstaVacio());
+        }
+
+        [Test]
+        public void testDeberiaNoEstarVacioCuandoSeAgregaAlgo()
+        {
+            Tablero tablero = new Tablero();
+            tablero.AgregarNave(new Nave(1, 1, new Posicion(1, 1), Nave.HORIZONTAL));
+
+            Assert.False(tablero.EstaVacio());
+        }
+
+        [Test]
+        public void testDeberiaHaberNaveEnUnaPosicionLuegoDeQueSeAgrego()
+        {
+            Tablero tablero = new Tablero();
+            tablero.AgregarNave(new Nave(1, 1, new Posicion(1, 1), Nave.HORIZONTAL));
+
+            Assert.True(tablero.HayNave(new Posicion(1, 1)));
+        }
+
+        [Test]
+        public void testDeberiaHaberArmamentoEnUnaPosicionLuegoDeQueSeAgrego()
+        {
+            Tablero tablero = new Tablero();
+            Armamento mina = ArmamentoFactory.CrearMinaPorContacto(new Posicion(3, 3));
+            tablero.Impactar(mina);
+
+            Assert.True(tablero.HayMina(new Posicion(3, 3)));
+        }
+
+        [Test]
+        public void testDeberiaActualizarseLaPosicionDeLaNave()
+        {
+            Tablero tablero = new Tablero();
+            Nave nave = new Nave(1, 1, new Posicion(5, 5), Nave.VERTICAL);
+            nave.SetDireccion(new Direccion(1, 0));
+            tablero.AgregarNave(nave);
+            tablero.ActualizarPosicionDeNaves();
+
+            Assert.True(tablero.HayNave(new Posicion(6, 5)));
+        }
+    }
+}
