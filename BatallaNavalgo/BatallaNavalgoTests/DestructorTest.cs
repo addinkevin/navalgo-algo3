d70c0290351376a08bd090c61cd8ee29fbca0cd2
addinkevin@gmail.com <addinkevin@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-06-08 18:08:30 +0000

    Update del repositorio.

---
+++ BatallaNavalgo/BatallaNavalgoTests/DestructorTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using BatallaNavalgo;
+
+namespace BatallaNavalgoTests
+{
+    [TestFixture]
+    class DestructorTest
+    {
+        [Test]
+        public void testNoDeberiaDestruirloSiLoAtacoConMinas()
+        {
+            Destructor destructor = new Destructor( new Posicion(3,3), Nave.VERTICAL);
+            
+            destructor.RecibirAtaque(new Mina(new Posicion(3,3)));
+            destructor.RecibirAtaque(new Mina(new Posicion(4,3)));
+            destructor.RecibirAtaque(new Mina(new Posicion(5,3)));
+
+            Assert.False(destructor.EstaDestruida());
+        }
+
+        [Test]
+        public void testDeberiaDestruirloSiLoAtacoConDisparosComunes()
+        {
+            Destructor destructor = new Destructor(new Posicion(3, 3), Nave.VERTICAL);
+
+            destructor.RecibirAtaque(new DisparoComun(new Posicion(3, 3)));
+            destructor.RecibirAtaque(new DisparoComun(new Posicion(4, 3)));
+            destructor.RecibirAtaque(new DisparoComun(new Posicion(5, 3)));
+
+            Assert.True(destructor.EstaDestruida());
+
+        }
+
+
+    }
+}
