8e14c2057f6e6c6a4fea1522488620320283bf3d
fernandezsantid@gmail.com <fernandezsantid@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-06-09 21:14:13 +0000

    Update de la clase NaveFactoryTest a partir de los cambios hechos en NaveFactory

--- trunk/BatallaNavalgo/BatallaNavalgoTests/NaveFactoryTest.cs
+++ trunk/BatallaNavalgo/BatallaNavalgoTests/NaveFactoryTest.cs
@@ -13,11 +13,10 @@ namespace BatallaNavalgoTests
         [Test]
         public void testCrearLanchaDevuelveNaveConDosCasillas()
         {
-            Posicion posicion = new Posicion(5,5);
-            int orientacion = 0;            
+            Posicion posicion = new Posicion(5,5);   
             int cantidadDePartes;
 
-            Nave lancha = NaveFactory.CrearLancha(posicion, orientacion);
+            Nave lancha = NaveFactory.CrearLancha(posicion);
             cantidadDePartes = lancha.GetPosiciones().Count;
             
             Assert.True(cantidadDePartes==2);          
@@ -27,10 +26,9 @@ namespace BatallaNavalgoTests
         public void testCrearDestructorDevuelveNaveConTresCasillas()
         {
             Posicion posicion = new Posicion(5, 5);
-            int orientacion = 0;
             int cantidadDePartes;
 
-            Destructor destructor = NaveFactory.CrearDestructor(posicion, orientacion);
+            Destructor destructor = NaveFactory.CrearDestructor(posicion);
             cantidadDePartes = destructor.GetPosiciones().Count;
 
             Assert.True(cantidadDePartes == 3);
@@ -40,10 +38,9 @@ namespace BatallaNavalgoTests
         public void testCrearBuqueDevuelveNaveConCuatroCasillas()
         {
             Posicion posicion = new Posicion(5, 5);
-            int orientacion = 0;
             int cantidadDePartes;
 
-            Buque buque = NaveFactory.CrearBuque(posicion, orientacion);
+            Buque buque = NaveFactory.CrearBuque(posicion);
             cantidadDePartes = buque.GetPosiciones().Count;
 
             Assert.True(cantidadDePartes == 4);
@@ -53,10 +50,9 @@ namespace BatallaNavalgoTests
         public void testCrearPortaAvionesDevuelveNaveConCincoCasillas()
         {
             Posicion posicion = new Posicion(5, 5);
-            int orientacion = 0;
             int cantidadDePartes;
 
-            Nave portaAviones = NaveFactory.CrearPortaAviones(posicion, orientacion);
+            Nave portaAviones = NaveFactory.CrearPortaAviones(posicion);
             cantidadDePartes = portaAviones.GetPosiciones().Count;
 
             Assert.True(cantidadDePartes == 5);
@@ -66,10 +62,9 @@ namespace BatallaNavalgoTests
         public void testCrearRompeHielosDevuelveNaveConTresCasillas()
         {
             Posicion posicion = new Posicion(5, 5);
-            int orientacion = 0;
             int cantidadDePartes;
 
-            Nave rompehielos = NaveFactory.CrearRompeHielos(posicion, orientacion);
+            Nave rompehielos = NaveFactory.CrearRompeHielos(posicion);
             cantidadDePartes = rompehielos.GetPosiciones().Count;
 
             Assert.True(cantidadDePartes == 3);
