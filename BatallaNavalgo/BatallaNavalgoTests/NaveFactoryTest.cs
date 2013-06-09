c4ec0e599a145633f91a036cdf7d99c7ee724b86
MPrystupiuk@gmail.com <MPrystupiuk@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-06-09 15:39:12 +0000

    Aglunos Tests de NaveFactoryTest

---
+++ BatallaNavalgo/BatallaNavalgoTests/NaveFactoryTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BatallaNavalgo;
+using NUnit.Framework;
+
+namespace BatallaNavalgoTests
+{
+    [TestFixture]
+    class NaveFactoryTest
+    {
+        [Test]
+        public void testCrearLanchaDevuelveNaveConDosCasillas()
+        {
+            Posicion posicion = new Posicion(5,5);
+            int orientacion = 0;            
+            int cantidadDePartes;
+
+            Nave lancha = NaveFactory.CrearLancha(posicion, orientacion);
+            cantidadDePartes = lancha.GetPosiciones().Count;
+            
+            Assert.True(cantidadDePartes==2);          
+        }
+
+        [Test]
+        public void testCrearDestructorDevuelveNaveConTresCasillas()
+        {
+            Posicion posicion = new Posicion(5, 5);
+            int orientacion = 0;
+            int cantidadDePartes;
+
+            Destructor destructor = NaveFactory.CrearDestructor(posicion, orientacion);
+            cantidadDePartes = destructor.GetPosiciones().Count;
+
+            Assert.True(cantidadDePartes == 3);
+        }
+
+        [Test]
+        public void testCrearBuqueDevuelveNaveConCuatroCasillas()
+        {
+            Posicion posicion = new Posicion(5, 5);
+            int orientacion = 0;
+            int cantidadDePartes;
+
+            Destructor buque = NaveFactory.CrearDestructor(posicion, orientacion);
+            cantidadDePartes = buque.GetPosiciones().Count;
+
+            Assert.True(cantidadDePartes == 4);
+        }
+
+        [Test]
+        public void testCrearPortaAvionesDevuelveNaveConCincoCasillas()
+        {
+            Posicion posicion = new Posicion(5, 5);
+            int orientacion = 0;
+            int cantidadDePartes;
+
+            Destructor portaAviones = NaveFactory.CrearDestructor(posicion, orientacion);
+            cantidadDePartes = portaAviones.GetPosiciones().Count;
+
+            Assert.True(cantidadDePartes == 5);
+        }
+
+        [Test]
+        public void testCrearRompeHielosDevuelveNaveConTresCasillas()
+        {
+            Posicion posicion = new Posicion(5, 5);
+            int orientacion = 0;
+            int cantidadDePartes;
+
+            Destructor rompehielos = NaveFactory.CrearDestructor(posicion, orientacion);
+            cantidadDePartes = rompehielos.GetPosiciones().Count;
+
+            Assert.True(cantidadDePartes == 3);
+        }
+
+
+    }
+}
