ac7dca4746894ba330905b86f43b4357cb70b6ab
MPrystupiuk@gmail.com <MPrystupiuk@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-06-09 14:39:00 +0000

    Agregue tests a MinaPorContactoTest.

--- BatallaNavalgo/BatallaNavalgoTests/MinaPorContactoTest.cs
+++ BatallaNavalgo/BatallaNavalgoTests/MinaPorContactoTest.cs
@@ -40,6 +40,40 @@ namespace BatallaNavalgoTests
             Assert.True(mina.EstaExplotado());
         }
 
+        [Test]
+        public void SiColisionaConVariasNavesTodasLasColisionadasRecibenImpacto()
+        {
+            /*En este caso las naves son puntuales para comprobar su destruccion total*/
+            MinaPorContacto mina = new MinaPorContacto();
+            Posicion posicion = new Posicion(5, 5);
+            Tablero tablero = new Tablero();
+            Nave nave1 = new Nave(1, 1, posicion, 0);
+            Nave nave2 = new Nave(1, 1, posicion, 0);            
+            tablero.AgregarNave(nave1);
+            tablero.AgregarNave(nave2);
+            mina.SetPosicion(posicion);
+            mina.SetTablero(tablero);
+
+            mina.Actualizar();
+            Assert.True(nave1.EstaDestruida() && nave2.EstaDestruida());
+        }
+
+        [Test]
+        public void SiHayNaveEnPosicionVecinaNoExplota()
+        {
+            /*En este caso las naves son puntuales para comprobar su destruccion total*/
+            MinaPorContacto mina = new MinaPorContacto();
+            Posicion posicionMina = new Posicion(5, 5);
+            Posicion posicionNave = new Posicion(6, 5);
+            Tablero tablero = new Tablero();
+            Nave nave1 = new Nave(1, 1, posicionNave, 0);            
+            tablero.AgregarNave(nave1);            
+            mina.SetPosicion(posicionMina);
+            mina.SetTablero(tablero);
+
+            mina.Actualizar();
+            Assert.False(mina.EstaExplotado());
+        }
 
 
     }
