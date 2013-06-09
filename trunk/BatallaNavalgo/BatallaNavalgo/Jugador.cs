f6ebd7071f130887dfeda5c6d588198d7345dd35
fernandezsantid@gmail.com <fernandezsantid@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-06-09 17:55:24 +0000

    Agregue la clase Jugador y JugadorTest

---
+++ trunk/BatallaNavalgo/BatallaNavalgo/Jugador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BatallaNavalgo
+{
+    public class Jugador
+    {
+        private int puntos;
+        private Tablero tablero;
+
+        /* Constructor
+         * puntos: seran los puntos con los que cuente el Jugador inicialmente.
+         * tablero: sera el tablero asociado que tendra el Jugador.
+         */
+        public Jugador(Tablero tablero)
+        {
+            this.puntos = 10000;
+            this.tablero = tablero;
+        }
+        //---------------------------------------------------------------------
+
+        public int GetPuntos()
+        {
+            return this.puntos;
+        }
+
+        public void DescontarPuntosPorPasoDeTurno()
+        {
+            puntos -= 10;
+        }
+
+        public void Disparar(Armamento armamento)
+        {
+            tablero.Impactar(armamento);
+            puntos -= (armamento.GetCosto());
+        }
+    }
+}
