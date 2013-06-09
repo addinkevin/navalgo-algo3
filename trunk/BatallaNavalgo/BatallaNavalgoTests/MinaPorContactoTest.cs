f4fd4c5566abc609fc862473c576a3dd5103133e
MPrystupiuk@gmail.com <MPrystupiuk@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-06-09 14:32:37 +0000

    Agregue MinaPorContactoTest.

---
+++ trunk/BatallaNavalgo/BatallaNavalgoTests/MinaPorContactoTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BatallaNavalgo;
+using NUnit.Framework;
+
+namespace BatallaNavalgoTests
+{
+    [TestFixture]
+    class MinaPorContactoTest
+    {
+        [Test]
+        public void testDeberiaNoEstarExplotadaAlCrearla()
+        {
+            MinaPorContacto mina = new MinaPorContacto();
+            Assert.False(mina.EstaExplotado());            
+        }
+
+        [Test]
+        public void NoDeberiaExplotarAlActualizar()
+        {
+            MinaPorContacto mina = new MinaPorContacto();
+            mina.Actualizar();
+            Assert.False(mina.EstaExplotado());
+        }
+
+        [Test]
+        public void DeberiaExplotarSiSuPosicionCoincideConLaDeUnaNave()
+        {
+            MinaPorContacto mina = new MinaPorContacto();
+            Posicion posicion = new Posicion(5,5);
+            Tablero tablero = new Tablero();
+            Nave nave = new Nave(1, 1, posicion, 0);
+            tablero.AgregarNave(nave);
+            mina.SetPosicion(posicion);
+            mina.SetTablero(tablero);
+
+            mina.Actualizar();
+            Assert.True(mina.EstaExplotado());
+        }
+
+
+
+    }
+}
