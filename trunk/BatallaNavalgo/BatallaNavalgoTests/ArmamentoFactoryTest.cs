b9c0b41f0d110e53f59dc67d1b7419e7f2d687e9
fernandezsantid@gmail.com <fernandezsantid@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-06-11 18:26:08 +0000

    Agregue mas test al ArmamentoFactoryTest

--- trunk/BatallaNavalgo/BatallaNavalgoTests/ArmamentoFactoryTest.cs
+++ trunk/BatallaNavalgo/BatallaNavalgoTests/ArmamentoFactoryTest.cs
@@ -20,8 +20,44 @@ namespace BatallaNavalgo
         }
 
         [Test]
+        /*Comprueba coste de creacion*/
+        public void testCreaUnaMinaPuntualConCostoValido()
+        {
+            Posicion posicion = new Posicion(3, 4);
+            MinaConRetardo mina = ArmamentoFactory.CrearMinaPuntual(posicion);
+            Assert.True(mina.GetCosto() == 50);
+        }
+
+        [Test]
+        /*Comprueba coste de creacion*/
+        public void testCreaUnaMinaDobleConCostoValido()
+        {
+            Posicion posicion = new Posicion(3, 4);
+            MinaConRetardo mina = ArmamentoFactory.CrearMinaDoble(posicion);
+            Assert.True(mina.GetCosto() == 100);
+        }
+
+        [Test]
+        /*Comprueba coste de creacion*/
+        public void testCreaUnaMinaTripleConCostoValido()
+        {
+            Posicion posicion = new Posicion(3, 4);
+            MinaConRetardo mina = ArmamentoFactory.CrearMinaTriple(posicion);
+            Assert.True(mina.GetCosto() == 125);
+        }
+
+        [Test]
+        /*Comprueba coste de creacion*/
+        public void testCreaUnaMinaPorContactoConCostoValido()
+        {
+            Posicion posicion = new Posicion(3, 4);
+            DisparoComun disparo = ArmamentoFactory.CrearDisparoComun(posicion);
+            Assert.True(disparo.GetCosto() == 150);
+        }
+
+        [Test]
         /*Comprueba radio*/
-        public void testCreaCrearCrearMinaPuntualValida()
+        public void testCreaMinaPuntualValida()
         {
             Posicion posicion = new Posicion(3, 4);
             MinaConRetardo mina = ArmamentoFactory.CrearMinaPuntual(posicion);
