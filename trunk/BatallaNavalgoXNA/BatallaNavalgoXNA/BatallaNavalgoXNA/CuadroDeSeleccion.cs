373d31b72d21fa2af686ae74e7184c11f6984be9
MPrystupiuk@gmail.com <MPrystupiuk@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-06-16 18:28:09 +0000

    Agregue los botones al menu (falta funcionalidad). Cree la clase CuadroDeSeleccion para quitarle responsabilidades que no le corresponden a MenuArmamentos

---
+++ trunk/BatallaNavalgoXNA/BatallaNavalgoXNA/BatallaNavalgoXNA/CuadroDeSeleccion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using BatallaNavalgo;
+
+namespace BatallaNavalgoXNA
+{
+    class CuadroDeSeleccion
+    {
+        private Texture2D imagenSeleccionado, imagenNoSeleccionado;
+        private Vector2 posicion;
+        private Boolean seleccionado;
+
+        public CuadroDeSeleccion(Vector2 posicion, Texture2D imagenSeleccionado, Texture2D imagenNoSeleccionado) 
+        {
+            this.posicion = posicion;
+            this.imagenNoSeleccionado = imagenNoSeleccionado;
+            this.imagenSeleccionado = imagenSeleccionado;
+            seleccionado = false;            
+        }
+
+        
+        public float X 
+        {
+            get { return posicion.X; }
+        }
+                
+        public float Y
+        {
+            get { return posicion.Y; }
+        }
+
+        public Texture2D Imagen 
+        {
+            get 
+            {
+                if (seleccionado)
+                    return imagenSeleccionado;
+                else
+                    return imagenNoSeleccionado;                
+            }
+        }
+
+    }
+}
--- trunk/BatallaNavalgoXNA/BatallaNavalgoXNA/BatallaNavalgoXNA/MenuArmamentos.cs
+++ trunk/BatallaNavalgoXNA/BatallaNavalgoXNA/BatallaNavalgoXNA/MenuArmamentos.cs
@@ -10,22 +10,37 @@ namespace BatallaNavalgoXNA
 {
     class MenuArmamentos
     {
-        public enum Disparos {DISPARO_COMUN, MINA_PUNTUAL, MINA_DOBLE, MINA_TRIPLE, MINA_POR_CONTACTO};
+        public enum MenuDisparos {NINGUNO, DISPARO_COMUN, MINA_PUNTUAL, MINA_DOBLE, MINA_TRIPLE, MINA_POR_CONTACTO};
 
         private Vector2 posicionInicialEnPantalla;
         private int cantidadDeLineas;
         private Texture2D botonSeleccionado, botonVacio;
         private SpriteFont fuente;
         private const int SALTO_DE_LINEA = 40;
+        private const int LADO_DE_BOTON = 24;
+        private MenuDisparos DisparoSeleccionado;
+        private Queue<CuadroDeSeleccion> botones;
 
 
-        public MenuArmamentos(Vector2 posicionMenuEnPantalla, Texture2D seleccionado, Texture2D vacio) 
-        {
-            posicionInicialEnPantalla = posicionMenuEnPantalla;            
-            botonSeleccionado = seleccionado;
-            botonVacio = vacio;
+        public MenuArmamentos(Vector2 posicionMenuEnPantalla)
+        {   
+            posicionInicialEnPantalla = posicionMenuEnPantalla;  
+            DisparoSeleccionado = MenuDisparos.NINGUNO;
+            botones = new Queue<CuadroDeSeleccion>();
+            
         }
 
+        public Texture2D ImagenDebotonVacio 
+        {
+            set { botonVacio = value; }
+        }
+
+        public Texture2D ImagenDebotonSeleccionado
+        {
+            set { botonSeleccionado = value; }
+        }
+
+        /*Dibuja el menu con los botones correspondientes*/
         public void Draw(SpriteBatch spriteBatch, SpriteFont fuente) 
         {
             cantidadDeLineas = 0;
@@ -38,14 +53,49 @@ namespace BatallaNavalgoXNA
             DibujarLinea("Mina doble: 100 ptos.", spriteBatch);
             DibujarLinea("Mina triple: 125 ptos.", spriteBatch);
             DibujarLinea("Mina contacto: 150 ptos.", spriteBatch);
+            CrearBotonesDeMenu(botonVacio, botonSeleccionado);
+            DibujarBloquesDeSeleccion(spriteBatch);
         }
 
         private void DibujarLinea(String texto, SpriteBatch spriteBatch)
         {
-            Vector2 vectorAuxiliar = new Vector2(posicionInicialEnPantalla.X, posicionInicialEnPantalla.Y + (SALTO_DE_LINEA * cantidadDeLineas));
-            spriteBatch.DrawString(fuente, texto, vectorAuxiliar, Color.White);
-           
+            int sangria = LADO_DE_BOTON + 10;
+            Vector2 vectorAuxiliar = new Vector2(posicionInicialEnPantalla.X + sangria, posicionInicialEnPantalla.Y + (SALTO_DE_LINEA * cantidadDeLineas));
+            spriteBatch.DrawString(fuente, texto, vectorAuxiliar, Color.White);           
             cantidadDeLineas++;
-        }            
+        }
+            
+
+        private void DibujarBloquesDeSeleccion(SpriteBatch spriteBatch)
+        {
+            IEnumerator<CuadroDeSeleccion> c = botones.GetEnumerator();
+            while (c.MoveNext()) 
+            {
+                int x = (int)c.Current.X;
+                int y = (int)c.Current.Y;
+                spriteBatch.Draw(c.Current.Imagen, new Rectangle(x, y, LADO_DE_BOTON, LADO_DE_BOTON), Color.White);
+            }
+        }
+
+        public void CrearBotonesDeMenu(Texture2D vacio, Texture2D seleccionado)
+        {
+            int tiposDeArmamento =1;
+            while (tiposDeArmamento <= 5)
+            {                
+                Vector2 posicionCorrespondienteDeBoton = new Vector2(posicionInicialEnPantalla.X, posicionInicialEnPantalla.Y + (SALTO_DE_LINEA * tiposDeArmamento));
+                CuadroDeSeleccion cuadroAuxiliar = new CuadroDeSeleccion(posicionCorrespondienteDeBoton, seleccionado, vacio);
+                botones.Enqueue(cuadroAuxiliar);
+                tiposDeArmamento++;
+            }
+        }
+
+
+        public void ActualizarSeleccion(int fila, int columna)
+        {
+            
+
+
+        }
+                
     }
 }
