3d4223006c8f9c12383e88aa2fb3c2e559403670
addinkevin@gmail.com <addinkevin@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-06-23 22:41:46 +0000

    Agrego interfaces Observador, y Observable.

---
+++ BatallaNavalgo/BatallaNavalgo/Observable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BatallaNavalgo
+{
+    public interface Observable
+    {
+        void AddObservador(Observador observador);
+        void NotificarObservadores();
+        void NotificarObservadoresDeCreacionDeLancha(Nave nave);
+        void NotificarObservadoresDeCreacionDeDestructor(Nave nave);
+        void NotificarObservadoresDeCreacionDePortaAviones(Nave nave);
+        void NotificarObservadoresDeCreacionDeRompeHielo(Nave nave);
+        void NotificarObservadoresDeCreacionDeBuque(Nave nave);
+    }
+}
---
+++ BatallaNavalgo/BatallaNavalgo/Observador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BatallaNavalgo
+{
+    public interface Observador
+    {
+        void NotificarCreacionDeLancha(Nave nave);
+        void NotificarCreacionDeDestructor(Nave nave);
+        void NotificarCreacionDePortaAviones(Nave nave);
+        void NotificarCreacionDeRompeHielo(Nave nave);
+        void NotificarCreacionDeBuque(Nave nave);
+        void Update();
+    }
+}
