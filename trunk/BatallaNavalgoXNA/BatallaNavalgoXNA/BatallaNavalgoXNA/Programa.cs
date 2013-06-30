51b05360160c52f1fee0f6c9a7a1eba67fb5e02d
addinkevin@gmail.com <addinkevin@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-06-30 19:36:18 +0000

    Algunos cambios en el proyecto.

---
+++ trunk/BatallaNavalgoXNA/BatallaNavalgoXNA/BatallaNavalgoXNA/Programa.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace BatallaNavalgoXNA
+{
+#if WINDOWS || XBOX
+    static class Programa
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (JuegoBatallaNavalgo game = new JuegoBatallaNavalgo())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
