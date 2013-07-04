60d6de929085a2870e5ce761336f95d56353d9b8
addinkevin@gmail.com <addinkevin@gmail.com@516428c9-db6a-d137-c09f-626cd0315003>
2013-07-04 02:08:18 +0000

    Tp terminado.

---
+++ trunk/TPUltimaEntrega/BatallaNavalgo/BatallaNavalgo/Tablero.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BatallaNavalgoExcepciones;
+
+namespace BatallaNavalgo
+{
+    public class Tablero
+    {
+        private static int CANTIDAD_DE_FILAS = 10;
+        private static int CANTIDAD_DE_COLUMNAS = 10;
+        public static Posicion ESQUINA_IZQUIERDA_ARRIBA = new Posicion(1, 1);
+        public static Posicion ESQUINA_DERECHA_ABAJO = new Posicion(10, 10);
+        private List<Armamento> armamentos;
+        private List<Nave> naves;
+        
+        /*Constructor del Tablero*/
+        public Tablero()
+        {            
+            armamentos = new List<Armamento>();
+            naves = new List<Nave>();
+        }
+
+        //---------------------------------------------------------------------
+        public static int Filas 
+        {
+            get { return CANTIDAD_DE_FILAS; }
+        }
+
+        public static int Columnas
+        {
+            get { return CANTIDAD_DE_COLUMNAS; }
+        }
+
+        /*Agrega una nave al tablero*/
+        public void AgregarNave(Nave nave) 
+        {
+            naves.Add(nave);
+        }
+
+        /*Agrega un armamento al tablero*/
+        public void Impactar(Armamento arma) 
+        {
+            if (!arma.Posicion.EstaDentroDe(Tablero.ESQUINA_IZQUIERDA_ARRIBA, Tablero.ESQUINA_DERECHA_ABAJO))
+                throw new ArmamentoFueraDelTableroException();
+
+            armamentos.Add(arma);
+        }
+
+        /*Obtiene las naves del tablero que estan en una posicion*/
+        public List<Nave> GetNavesEn(Posicion posicion)
+        {
+            List<Nave> listaNavesEnPosicion = new List<Nave>();
+            foreach (Nave nave in naves)
+            {
+                List<Posicion> listaPosicionesDePartes = (nave.GetPosiciones());
+                foreach (Posicion posicionParteNave in listaPosicionesDePartes)
+                {
+                    if (posicion.EsIgualA(posicionParteNave))
+                    {
+                        listaNavesEnPosicion.Add(nave);
+                    }
+                }
+            }
+            return listaNavesEnPosicion;
+        }
+
+        //---------------------------------------------------------------------
+
+        /* Actualización del tablero.
+         * Primero actualiza los armamentos, de forma de hacer que exploten si tienen que explotar,
+         * Y luego mueve las naves */
+        public void Actualizar() 
+        {
+            this.ActualizarArmamentos();
+            this.BorrarArmamentosExplotados();
+            this.ActualizarPosicionDeNaves();
+        }
+
+        /* Método auxiliar para mover las naves */
+        private void ActualizarPosicionDeNaves()
+        {
+            foreach (Nave nave in naves)
+            {
+                nave.Mover();
+            }
+        }
+
+        /* Método auxiliar para actualizar los armamentos */
+        private void ActualizarArmamentos()
+        {
+            foreach (Armamento armamento in armamentos)
+            {
+                armamento.Actualizar();
+            }
+        }
+
+        /* Método auxiliar para borrar los armamentos que ya están explotados y no sirven */
+        private void BorrarArmamentosExplotados()
+        {
+            List<Armamento> armamentosActualizados = new List<Armamento>();
+            foreach (Armamento armamento in armamentos)
+            {
+                if (!armamento.Explotado)
+                {
+                    armamentosActualizados.Add(armamento);
+                }
+            }
+            this.armamentos = armamentosActualizados;
+        }
+
+        //---------------------------------------------------------------------
+        /*Si el tablero sigue teniendo alguna nave*/
+        public Boolean HayNave(Posicion posicion) 
+        {
+            foreach (Nave nave in naves)
+            {
+                List<Posicion> listaPosiciones = (nave.GetPosiciones());
+                foreach (Posicion posicionParteNave in listaPosiciones)
+                {
+                    if (posicion.EsIgualA(posicionParteNave))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        /*Estado de las naves del tablero*/
+        public Boolean TieneNavesConVida()
+        {
+            foreach (Nave nave in naves)
+            {
+                if (!nave.EstaDestruida())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ trunk/TPUltimaEntrega/BatallaNavalgo/BatallaNavalgoXNA/VistaTablero.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using BatallaNavalgo;
+
+namespace BatallaNavalgoXNA
+{
+    public class VistaTablero
+    {
+        private static Vector2 POSICION_TABLERO_EN_PANTALLA = new Vector2(400, 80);
+        public static int TAMANIO_BLOQUE_DEL_TABLERO = 40;
+
+        private int altoTablero;
+        private int anchoTablero;
+        private int posicionInicialTableroEnX;
+        private int posicionInicialTableroEnY;
+        private int posicionFinalTableroEnX;
+        private int posicionFinalTableroEnY;
+
+        public VistaTablero(int altoTablero, int anchoTablero)
+        {
+            this.altoTablero = altoTablero;
+            this.anchoTablero = anchoTablero;
+            posicionInicialTableroEnX = (int)POSICION_TABLERO_EN_PANTALLA.X;
+            posicionInicialTableroEnY = (int)POSICION_TABLERO_EN_PANTALLA.Y;
+            posicionFinalTableroEnX = posicionInicialTableroEnX + (TAMANIO_BLOQUE_DEL_TABLERO * anchoTablero);
+            posicionFinalTableroEnY = posicionInicialTableroEnY + (TAMANIO_BLOQUE_DEL_TABLERO * altoTablero);
+        }
+
+        public int PosicionInicialTableroEnX
+        {
+            get { return posicionInicialTableroEnX; }
+        }
+
+        public int PosicionInicialTableroEnY
+        {
+            get { return posicionInicialTableroEnY; }
+        }
+        
+        public Boolean EstaDentroDelTablero(int x, int y)
+        {
+            if ((y > posicionInicialTableroEnY) && (y < posicionFinalTableroEnY))
+            {
+                if ((x > posicionInicialTableroEnX) && (x < posicionFinalTableroEnX))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Texture2D bloqueTablero, SpriteFont fuenteBatallaNavalgo)
+        {
+            for (int i = 0; i < altoTablero; i++)
+            {
+                for (int j = 0; j < anchoTablero; j++)
+                {
+                    int ancho = posicionInicialTableroEnX + (i * TAMANIO_BLOQUE_DEL_TABLERO);
+                    int alto = posicionInicialTableroEnY + (j * TAMANIO_BLOQUE_DEL_TABLERO);
+                    spriteBatch.Draw(bloqueTablero, new Rectangle(ancho, alto, TAMANIO_BLOQUE_DEL_TABLERO, TAMANIO_BLOQUE_DEL_TABLERO), null, Color.White);
+                }
+            }
+            DibujarPosicionesDelTablero(spriteBatch, fuenteBatallaNavalgo);
+        }
+
+        public void DibujarPosicionesDelTablero(SpriteBatch spriteBatch, SpriteFont fuenteBatallaNavalgo)
+        {
+            int posicionInicialEnX = posicionInicialTableroEnX + 10; //Se suma el 10 en ambas posiciones
+            int posicionInicialEnY = posicionInicialTableroEnY + 10; //solo por un tema de vista.
+            for (int i = 0; i < Tablero.Filas; i++)
+            {
+                spriteBatch.DrawString(fuenteBatallaNavalgo, "" + (i + 1), new Vector2(375, posicionInicialEnY), Color.White);
+                posicionInicialEnY += 40;              
+            }
+            for (int j = 0; j< Tablero.Columnas; j++)
+            {
+                spriteBatch.DrawString(fuenteBatallaNavalgo, "" + (j + 1), new Vector2(posicionInicialEnX, 55), Color.White);
+                posicionInicialEnX += 40;
+            }
+        }
+
+        public Vector2 GetPosicionDe(int fila, int columna)
+        {
+            Vector2 posicionEnPantalla = new Vector2(0,0);
+            posicionEnPantalla.X = (posicionInicialTableroEnX + 10) +
+                                    ((columna - 1) * VistaTablero.TAMANIO_BLOQUE_DEL_TABLERO); //Se suma el 10 en ambas posiciones
+            posicionEnPantalla.Y = (posicionInicialTableroEnY + 10) + 
+                                    ((fila - 1) * VistaTablero.TAMANIO_BLOQUE_DEL_TABLERO); //solo por un tema de vista.
+
+            return posicionEnPantalla;
+            
+        }       
+    }
+}
